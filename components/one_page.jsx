3a2a2dad84f52c3286bd667d43f54adfb7206173
Will Sommers <will.sommers@gmail.com>
2016-02-06 19:57:36 -0500

    level_one

--- components/one_page.jsx
+++ components/one_page.jsx
@@ -1,9 +1,71 @@
 var React = require("react");
+var _ = require("underscore");
 
-var one_page = React.createClass({
+
+var ChildElement = React.createClass({
+
+  getInitialState: function() {
+    return {
+      isChanging: false
+    }
+  },
+
+  componentDidMount: function() {
+    var ctx = this;
+    if (this.props.number === 3) {
+      window.setInterval(function() {
+        ctx.setState({ isChanging: !ctx.state.isChanging});
+      }, 3000);
+    }
+  },
   
   render: function() {
-    return <div> Hi </div>
+    console.log(this.props.number) 
+    return <div onClick={this.props.flipColor}>
+      <div>
+        {this.props.number}
+      </div>
+      <div style={{color: this.props.childColor}}>
+        {this.state.isChanging.toString()}
+      </div>
+    </div>
+  }
+})
+
+var one_page = React.createClass({
+  getInitialState: function() {
+    return {
+      textColor: "red"
+    }
+  },
+
+  flipColor: function() {
+    var color;
+    if (this.state.textColor === "red") {
+      color = "blue"
+    } else {
+      color = "red"
+    }
+    this.setState({ textColor: color});
+  },
+
+  render: function() {
+    var ctx = this;
+    return <div>
+      {_.map([1,2,3,4,5], function(i) {
+        var childColor;
+        if (i < 3) {
+          childColor = ctx.state.textColor;
+        } 
+
+        return <ChildElement 
+          number={i}
+          key={i}
+          childColor={childColor}
+          flipColor={ctx.flipColor}
+        />
+      })}
+    </div>
   }
 })
 
